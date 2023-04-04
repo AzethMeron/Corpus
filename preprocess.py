f9cabbcc04ddbd74a80969c5a92bd363c5e27e50
AzethM <meronentertainment@gmail.com>
2023-04-04 17:15:10 +0200

    Initial version of preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,15 +1,13 @@
 
 import re
 import nltk
+import string
+from nltk.tokenize import word_tokenize
 
 # Does it make sense to make preprocessor a class, since it will mosty likely have only constant attributes, which could be just global variables?
 # Probably not, although if at some point necessity for an internal variable for instance appears, class objects will allow it and global variables would not
 class Preprocessor:
     def __init__(self):
-        self.__tokenizer = None
-        self.__lemmatizer = None
-        self.__stopwords = None
-        self.__interpuntion = None
         self.__token_url = "URL"
         self.__token_user = "USER"
     def __strip_urls(self, text):
@@ -18,9 +16,34 @@ class Preprocessor:
         for url in urls:
             text = text.replace(url[0], self.__token_url)
         return text
-    def Tokenize(self, text):
-        pass
+    def __strip_users(self, text):
+        for word in text.split():
+            if(word[0] == "@"):
+                text = text.replace(word, self.__token_user)
+        return text
+    def __strip_stopwords(self, text):
+        stopwords = set(nltk.corpus.stopwords.words('english'))
+        words = []
+        for word in self.WordTokenize(text):
+            if word not in stopwords:
+                words.append(word)
+        return ' '.join(words)
+    def __lemmatize(self, text):
+        words = []
+        lemmatize = nltk.WordNetLemmatizer().lemmatize
+        for word in self.WordTokenize(text):
+            words.append( lemmatize(word) )
+        return ' '.join(words)
+    def __strip_punctuation(self, text):
+        punctuation = string.punctuation
+        return text.translate(str.maketrans('', '', punctuation))
+    def WordTokenize(self, text):
+        return word_tokenize(text)
     def Preprocess(self, text):
         text = text.lower()
-        #
+        text = self.__strip_urls(text)
+        text = self.__strip_users(text)
+        text = self.__lemmatize(text)
+        text = self.__strip_punctuation(text)
+        text = self.__strip_stopwords(text)
         return text
